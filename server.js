847729a6ec95028a0ba283c0f83dd550b8e14356
gaurav-kasaudhan786 <gskasaudhan456@gmail.com>
2026-02-27 22:49:19 +0530

    Adding DB Configration

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const authRoutes = require("./routes/auth");
+const profileRoutes = require("./routes/profile");
+const listingRoutes = require("./routes/listing");
+const bookingRoutes = require("./routes/bookings");
 
 const app = express();
 app.use(cors());
-app.use(express.json());        
-
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Api is running...");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/api/listings", listingRoutes);
+app.use("/api/bookings", bookingRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Mongodb Connected"))
+  .catch((err) => console.error(err));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
